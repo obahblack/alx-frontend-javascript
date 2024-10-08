21c351af434a607bdab6a2e6832c854ae1374a0c
Praise Obah <Obahsomto@gmail.com>
2024-10-08 09:44:13 +0100

    data arrays

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const objArrray = [];
+  objArrray.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  objArrray.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  objArrray.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+
+  return objArrray;
+}

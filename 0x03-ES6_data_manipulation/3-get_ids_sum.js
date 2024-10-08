9734eae6ba116465c1dd1abc958ed6e10c494da0
Praise Obah <Obahsomto@gmail.com>
2024-10-08 11:20:56 +0100

    correction to reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum (students) {
-  const sum = students.reduce((acc, num) => acc + num, 0);
+  const sum = students.reduce((acc, num) => acc + num.id, 0);
   return sum;
 }
